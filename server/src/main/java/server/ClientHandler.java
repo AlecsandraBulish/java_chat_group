578025d391d2b66b65d7ae09a7b0d841b2078b00
AlexBulish <alecsandra-bulish>
2021-04-25 18:07:17 +0300

    HW for 4th class

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.sql.SQLException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ClientHandler {
     private Server server;
@@ -16,6 +19,7 @@ public class ClientHandler {
     private DataOutputStream out;
     private String nickName;
     private String login;
+    private ExecutorService service;
 
     public ClientHandler(Server server, Socket socket) {
         try {
@@ -23,8 +27,9 @@ public class ClientHandler {
             this.socket = socket;
             in = new DataInputStream(socket.getInputStream());
             out = new DataOutputStream(socket.getOutputStream());
+            service = Executors.newFixedThreadPool(4);
 
-            new Thread(() -> {
+            service.execute(() -> {
                 try {
                     socket.setSoTimeout(120000);
                     // цикл аутоинтефикации
@@ -115,10 +120,106 @@ public class ClientHandler {
                         e.printStackTrace();
                     }
                 }
-            }).start();
+            });
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+//            new Thread(() -> {
+//                try {
+//                    socket.setSoTimeout(120000);
+//                    // цикл аутоинтефикации
+//                    while (true) {
+//                        String str = in.readUTF();
+//
+//                        if (str.equals(Command.END)) {
+//                            out.writeUTF(Command.END);
+//                            throw new RuntimeException("Клиент захотел отключиться");
+//                        }
+//
+//                        if (str.startsWith(Command.AUTH)) {
+//                            String[] token = str.split(" ",3);
+//                            if (token.length < 3) {
+//                                continue;
+//                            }
+//                            String newNick = server.getAuthService().getNickByLoginAndPassword(token[1], token[2]);
+//
+//                            System.out.println(nickName);
+//                            if (newNick != null) {
+//                                login = token[1];
+//                                if (!server.isLoginAuthenticated(login)) {
+//                                    nickName = newNick;
+//                                    sendMsg(Command.AUTH_OK + " " + nickName);
+//                                    server.subscribe(this);
+//                                    System.out.println("client: " + socket.getRemoteSocketAddress() + " connected with nick: " + nickName);
+//                                    socket.setSoTimeout(0);
+//                                    break;
+//                                } else {
+//                                    sendMsg("Данная учетка уже занята");
+//                                }
+//
+//                            } else {
+//                                sendMsg("Wrong login or password");
+//                            }
+//                        }
+//                        if (str.startsWith(Command.REG)) {
+//                            String[] token = str.split(" ", 4);
+//                            if (token.length < 4) {
+//                                continue;
+//                            }
+//                            boolean regSuccess = server.getAuthService().registration(token[1], token[2], token[3]);
+//                            if (regSuccess) {
+//                                sendMsg(Command.REG_OK);
+//                            } else {
+//                                sendMsg(Command.REG_NO);
+//                            }
+//                        }
+//                    }
+//                    // цикл работы
+//                    while (true) {
+//                        String str = in.readUTF();
+//
+//                        if (str.startsWith(Command.SERVICE_MSG)) {
+//                            if (str.equals(Command.END)) {
+//                                out.writeUTF(Command.END);
+//                                break;
+//                            }
+//                            if (str.startsWith(Command.PRIVATE_MSG)) {
+//                                String[] text = str.split(" ", 3);
+//                                if (text.length < 3) {
+//                                    continue;
+//                                }
+//                                server.sendMsgPrivate(this, text[1], text[2]);
+//                            }
+//                        } else {
+//                            server.broadcastMsg(this, str);
+//                        }
+//                    }
+//                } catch (SocketTimeoutException e) {
+//                    System.out.println("Время вышло");
+//                    try {
+//                        out.writeUTF(Command.END);
+//                    } catch (IOException ioException) {
+//                        ioException.printStackTrace();
+//                    }
+//                } catch (RuntimeException e) {
+//                    System.out.println(e.getMessage());
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                } finally {
+//
+//                    server.unsubscribe(this);
+//                    System.out.println("Client disconnected: " + nickName);
+//                    try {
+//                        socket.close();
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            }).start();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public String getNickName() {
